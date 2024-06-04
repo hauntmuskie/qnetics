7a1ab52594a6660e22846e5de3a4084f6c05c32c
Rionggo Rahardi <rioclasher4@gmail.com>
2024-06-04 20:39:54 +0700

    chore: Update tailwind.config.ts with new animation styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,16 +18,6 @@ const config: Config = {
         "meteor-effect": "meteor 5s linear infinite",
       },
       keyframes: {
-        spotlight: {
-          "0%": {
-            opacity: 0,
-            transform: "translate(-72%, -62%) scale(0.5)",
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translate(-50%,-40%) scale(1)",
-          },
-        },
         meteor: {
           "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
           "70%": { opacity: "1" },
